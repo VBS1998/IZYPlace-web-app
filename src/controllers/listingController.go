4a7d10f1ef39dbe12c36b0e3405672eea0867f94
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2024-10-18 16:38:31 -0300

    Listing controller and server added

---
+++ src/controllers/listingController.go
@@ -0,0 +1,52 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/VBS1998/base-web-app/src/models"
+	"github.com/gorilla/mux"
+)
+
+func GetListings(w http.ResponseWriter, r *http.Request) {
+	listings := models.ListingsMock()
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(listings)
+
+	if err != nil {
+		// Handle encoding error if any
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
+}
+
+func GetListing(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	for _, listing := range models.ListingsMock() {
+		if listing.ID == id {
+			err := json.NewEncoder(w).Encode(listing)
+
+			if err != nil {
+				// Handle encoding error if any
+				http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+
+}
+
+func AddListing(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateListing(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteListing(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ src/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/VBS1998/base-web-app/src/controllers"
+	"github.com/gorilla/mux"
+)
+
+const (
+	LISTINGS_ROUTE         = "/listings"
+	LISTINGS_ROUTE_WITH_ID = "/listings/{id}"
+)
+
+type IZYServer struct {
+	Router          *mux.Router
+	AnonymousRoutes *mux.Router
+}
+
+func (r *IZYServer) setupAnonymousRoutes(router *mux.Router) {
+	anonymousRoutes := router.PathPrefix("/api/").Subrouter()
+
+	anonymousRoutes.HandleFunc(LISTINGS_ROUTE, controllers.GetListings).Methods(http.MethodGet)
+	anonymousRoutes.HandleFunc(LISTINGS_ROUTE_WITH_ID, controllers.GetListing).Methods(http.MethodGet)
+	anonymousRoutes.HandleFunc(LISTINGS_ROUTE, controllers.AddListing).Methods(http.MethodPost)
+	anonymousRoutes.HandleFunc(LISTINGS_ROUTE_WITH_ID, controllers.UpdateListing).Methods(http.MethodPut)
+	anonymousRoutes.HandleFunc(LISTINGS_ROUTE_WITH_ID, controllers.DeleteListing).Methods(http.MethodDelete)
+
+	r.AnonymousRoutes = anonymousRoutes
+
+}
+
+func (c *IZYServer) setupStaticFiles(router *mux.Router) {
+	router.PathPrefix("/").Handler(
+		http.StripPrefix("/",
+			http.FileServer(http.Dir("../frontend/out")),
+		),
+	)
+}
+
+func CreateServer() *IZYServer {
+
+	r := IZYServer{
+		Router: mux.NewRouter(),
+	}
+
+	r.setupAnonymousRoutes(r.Router)
+	r.setupStaticFiles(r.Router)
+
+	return &r
+}
