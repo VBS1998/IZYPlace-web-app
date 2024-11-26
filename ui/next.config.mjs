6f01a34c0a4163a0808c5b94afe569c0ce07f289
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2024-11-25 21:40:23 -0300

    Fixing warning

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'izyplace']
+        remotePatterns: [
+            { hostname: 'localhost' }, 
+            { hostname: 'izyplace' }
+        ]
     }
-    
 };
 
 export default nextConfig;
