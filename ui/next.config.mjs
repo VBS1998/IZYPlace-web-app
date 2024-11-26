fe0191372e69e096cc81996ee8bbdb97c28391ea
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2024-11-25 21:04:38 -0300

    Images in local folder with volumes (#7)

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['theknot.com', 'nationaltoday.com', 'img.freepik.com', 'www.fullerholidays.com.au', 'www.editionhotels.com', 'media.architecturaldigest.com']
+        domains: ['localhost', 'izyplace']
     }
     
 };
