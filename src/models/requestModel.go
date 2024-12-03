ed98f593b449ad9b35d625444494e1cd4c818cd7
Gustavo Vilas Boas <gustavovbs98@gmail.com>
2024-12-02 21:47:47 -0300

    Improving request model

--- src/models/requestModel.go
+++ src/models/requestModel.go
@@ -1,14 +1,8 @@
 package models
 
 type Request struct {
-	ID           string        `json:"id,omitempty" bson:"_id,omitempty"`
-	Name         string        `json:"name,omitempty" bson:"name,omitempty"`
-	Description  string        `json:"description,omitempty" bson:"description,omitempty"`
-	Capacity     float64       `json:"capacity,omitempty" bson:"capacity,omitempty"`
-	Location     string        `json:"location,omitempty" bson:"location,omitempty"`
-	PricePerHour float64       `json:"pricePerHour,omitempty" bson:"pricePerHour,omitempty"`
-	Rating       float64       `json:"rating,omitempty" bson:"rating,omitempty"`
-	ImageURL     []string      `json:"imageUrl,omitempty" bson:"imageUrl,omitempty"`
-	Owner        Owner         `json:"owner,omitempty" bson:"owner,omitempty"`
-	Status       RequestStatus `json:"status,omitempty" bson:"status,omitempty"`
+	ID      string        `json:"id,omitempty" bson:"_id,omitempty"`
+	Listing Listing       `json:"listing,omitempty" bson:"listing,omitempty"`
+	Owner   Owner         `json:"owner,omitempty" bson:"owner,omitempty"`
+	Status  RequestStatus `json:"status,omitempty" bson:"status,omitempty"`
 }
